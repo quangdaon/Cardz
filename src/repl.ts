3a556de277d85f5813a6958d5c7e8516db3200b9
QuangdaoN <quangdaonguyen@graydientcreative.com>
2019-11-25 13:57:34 -0600

    REPL

---
+++ src/repl.ts
@@ -0,0 +1,7 @@
+import Deck from './definitions/Deck';
+import PlayingCardDeck from './implementations/playing-cards/PlayingCardDeck';
+
+// @ts-ignore
+global.Deck = Deck;
+// @ts-ignore
+global.PlayingCardDeck = PlayingCardDeck;
